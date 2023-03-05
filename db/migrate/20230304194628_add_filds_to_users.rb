30b1f90c372dfd3e34d355a57d7c51efa4afe8ae
mariaeduardamachado <mariaeduarda0450@gmail.com>
2023-03-05 10:36:53 -0300

    commit

---
+++ db/migrate/20230304194628_add_filds_to_users.rb
@@ -0,0 +1,9 @@
+class AddFildsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :phone, :string
+    add_column :users, :cpf, :string
+    add_column :users, :rg, :string
+    add_column :users, endereco, :string
+  end
+end
